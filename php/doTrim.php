bf9a08e5fce1b81a4c04a3f774cd0ecaa3e79cb1
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2020-03-02 13:30:10 +0330

    Use python3

--- php/doTrim.php
+++ php/doTrim.php
@@ -4,12 +4,12 @@ $trimStart = $_GET['trimStart'];
 $trimEnd = $_GET['trimEnd'];
 $copyrightChannel = $_GET['copyrightChannel'];
 
-echo "python main.py $fileAddress $trimStart $trimEnd $copyrightChannel -y <br>";
+echo "python3 main.py $fileAddress $trimStart $trimEnd $copyrightChannel <br>";
 
 
 //$cmd = "ping 127.0.0.1  > log.txt &"; //example command
 //$cmd = "python ../python/main.py $fileAddress $trimStart $trimEnd $copyrightChannel -y  2>log.txt &";
-$cmd = "python main.py $fileAddress $trimStart $trimEnd $copyrightChannel -y  2>&1";
+$cmd = "python3 main.py $fileAddress $trimStart $trimEnd $copyrightChannel 2>&1";
 
 echo "<br>";
 echo chdir("../python");
